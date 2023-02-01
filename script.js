bd24926b510ef2080a151b8ed26555e780442d68
Doricaa <doraugrin@yahoo.com>
2023-02-01 18:56:08 +0100

    Added changes

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function printLines() {
   var secondLine = "I'm your second line";
   console.log(secondLine);
 }
+
+//matrix //
+
+var matrix = [
+  [1, 2, 3],
+  ["John", "Jane", "Mike"],
+];
+
+matrix[0][1];
+matrix[2][2];
+matrix[0][4] = 5;
+matrix[1] = [];
