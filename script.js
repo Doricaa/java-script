97ca01fb468bbb1d2f2aa1a863afc05c3fc3b47a
Doricaa <doraugrin@yahoo.com>
2023-02-07 21:17:01 +0100

    Added changes

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const descriptionEl = document.querySelector("description");
+const descriptionEl = document.querySelector(".description");
+const footerEl = document.querySelector("#footer");
+const ulEl = document.querySelector("ul");
 
 const h1 = document.createElement("h1");
-h1.innerText = "Zbilja smo jako pametni";
+h1.innerText = "Zbilja smo jako smo pametni!";
+descriptionEl.parentNode.prepend(h1);
 
 const infoDiv = document.createElement("div");
 infoDiv.classList.add("info");
 footerEl.parentNode.insertBefore(infoDiv, footerEl);
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  ulEl.appendChild(li);
+}
